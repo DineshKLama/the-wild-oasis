a8e07610cac31c1a017e3c9690909bfb5e3bf78a
Dinesh_Lama <lakpadlama3@gmail.com>
2023-10-25 21:45:54 +0530

    working on bookings

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import { useCabins } from './useCabins';
 import Table from '../../ui/Table';
 import Menus from '../../ui/Menus';
 import { useSearchParams } from 'react-router-dom';
+import Empty from '../../ui/Empty';
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
@@ -30,6 +31,8 @@ function CabinTable() {
 
   // Loading state
   if (isLoading) return <Spinner />;
+
+  if (!cabins?.length) return <Empty resourceName='bookings' />;
 
   return (
     <Menus>
