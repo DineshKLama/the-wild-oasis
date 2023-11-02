c59afb9b0b7cac10848adb915ff8d83c8d0a09da
Dinesh Lama <dineshklama3@gmail.com>
2023-11-02 11:11:22 +0530

    test

--- src/features/authentication/useUser.js
+++ src/features/authentication/useUser.js
@@ -1,15 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
-import { getCurrentUser } from '../../services/apiAuth';
+import { useQuery } from "@tanstack/react-query";
+import { getCurrentUser } from "../../services/apiAuth";
 
 export function useUser() {
   const { data: user, isLoading } = useQuery({
-    queryKey: ['user'],
+    queryKey: ["user"],
     queryFn: getCurrentUser,
   });
 
   return {
     user,
     isLoading,
-    isAuthenticated: user?.role === 'authenticated',
+    isAuthenticated: user?.role === "authenticated",
   };
 }
+
+("hello");
